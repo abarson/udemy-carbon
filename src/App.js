a7d949d4e7be86ff74193846417b66bf839a564f
Adam Barson <adam.tyler.barson@ibm.com>
2019-09-23 17:55:00 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age:28 },
+      { name: 'Adam', age:23 },
+      { name: 'Steph', age:25 }
+    ]
+  }
+
+  //not activally calling, only using it to handle
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app</h1>
+        <p>this is really working</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
+  }
+}
+
+export default App;
